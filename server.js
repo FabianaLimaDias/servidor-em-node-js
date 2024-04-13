1e647aaa7c537bd8970a5a1418c729c569321311
FabianaDias <129968421+FabianaLimaDias@users.noreply.github.com>
2024-04-13 09:41:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require ("express")
-
-const porta = 333
+const express = require("express")
+const { v4: uuidv4 } = require('uuid')
+const router = express.Router()
 const app = express()
-function mostraPorta()
-{
-console.log("Servidor criado e rodando na porta", porta)
+const porta = 3333
+
+//POST
+function criaMulher(req, response) {
+    const novaMulher = {
+        id: uuidv4(),
+        nome: req.body.nome,
+        imagem: req.body.imagem,
+        minibio: req.body.minibio
+    }
+    
+    mulheres.push(novaMulher)
+
+    response.json(mulheres)
+}
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta ", porta)
 }
 
-mostraPorta()
+app.use(router.post('/mulheres', criaMulher))
 
-app.listen(porta,mostraPorta)
-
+app.listen(porta, mostraPorta)
