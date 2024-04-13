a6143e26b39cccb3d7d6de9adae46d0d7905df9d
FabianaDias <129968421+FabianaLimaDias@users.noreply.github.com>
2024-04-13 09:36:44 -0300

    Update mulher.js

--- mulher.js
+++ mulher.js
@@ -1,24 +1,20 @@
-const express = require("express");
-const router = express.Router(); //rota
+const express = require("express")
+const router = express.Router()
 
-const app = express();
-const porta = 3333;
+const app = express()
+const porta = 3333
 
 function mostraMulher(request, response) {
-  //chamar require e response
-  //objeto //json envia varias informações
-  response.json({
-    nome: "Mulher",
-    imagem: "https://www.designi.com.br/681ec2a35f63b25a",
-    minibio: "forte e corajosa",
-  });
+ response.json({
+    nome: 'Simara Conceição',
+    imagem: 'https://github.com/simaraconceicao.png',
+    minibio: 'Desenvolvedora e instrutora'
+ })
 }
 
 function mostraPorta() {
-  console.log("Servidor criado e rodando na porta", porta);
+    console.log("Servidor criado e rodando na porta ", porta)
 }
 
-app.use(router.get("/mulher", mostraMulher));
-app.listen(porta, mostraPorta);
-
-mostraPorta();
+app.use(router.get('/mulher', mostraMulher))
+app.listen(porta, mostraPorta)
